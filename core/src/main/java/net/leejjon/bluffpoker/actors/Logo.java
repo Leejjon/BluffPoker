13c649aedf6306721ca21b20470c2881f793e1bd
Leejjon <leejjon@gmail.com>
2017-11-13 23:03:13 +0100

    Should fix the layout problem in the start screen.

--- core/src/main/java/net/leejjon/bluffpoker/actors/Logo.java
+++ core/src/main/java/net/leejjon/bluffpoker/actors/Logo.java
@@ -2,15 +2,22 @@ package net.leejjon.bluffpoker.actors;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import net.leejjon.bluffpoker.BluffPokerGame;
+import net.leejjon.bluffpoker.stages.GameStage;
 
-public class Logo extends Image {
-    private Texture logoTexture;
+public class Logo extends Table {
+    private Image logo;
 
     public Logo(Texture logoTexture) {
-        super(logoTexture);
-        this.logoTexture = logoTexture;
+        setFillParent(false);
+        logo = new Image(logoTexture);
 
-        setWidth(logoTexture.getWidth() / 4);
-        setHeight(logoTexture.getHeight() / 4);
+        int middleX = (GameStage.getMiddleX() / BluffPokerGame.getDivideScreenByThis());
+        int firstQuarter = (GameStage.getMiddleY() / BluffPokerGame.getDivideScreenByThis());// - ((logoTexture.getHeight()) / 4) / 2;
+        setPosition(middleX, firstQuarter);
+
+        add(logo).width(logoTexture.getWidth() / 4).height(logoTexture.getHeight() / 4).padBottom(10f);
+        row();
     }
 }
